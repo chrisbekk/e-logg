3b8044f30edc19f9ad53e6fc05f7f2584449e28e
chrisbekk <christianbekkelund@gmail.com>
2024-10-23 14:45:37 +0200

    Minor adjustments to the header and sidebar width and height

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ type HeaderProps = { title: string };
 
 export const Header = ({ title }: HeaderProps) => {
   return (
-    <header className="grid__layout__header p-4">
+    <header className="grid__layout__header">
       <div>
         <h1 className="font-semibold text-2xl">{title}</h1>
       </div>
